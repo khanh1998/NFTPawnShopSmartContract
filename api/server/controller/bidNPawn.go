d552c68019809fa6b08e2ca4ea8dcd7f8d36a536
Khanh <quockhanh.abc@gmail.com>
2021-08-29 16:47:05 +0700

    refactor: remove unused function

--- api/server/controller/bidNPawn.go
+++ api/server/controller/bidNPawn.go
@@ -1,10 +1,6 @@
 package controller
 
 import (
-	"context"
-	"log"
-	"time"
-
 	"github.com/gin-gonic/gin"
 	"github.com/uss-kelvin/NFTPawningShopBackend/server/model"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -43,49 +39,4 @@ func (b *BidNPawnController) InsertBidToPawn(c *gin.Context, sc mongo.SessionCon
 		return err
 	}
 	return nil
-}
-
-func (b *BidNPawnController) InsertBidToPawnWithSession(connection *mongo.Client) gin.HandlerFunc {
-	fn := func(c *gin.Context) {
-		var session mongo.Session
-		var err error
-		if session, err = connection.StartSession(); err != nil {
-			log.Panic(err)
-		}
-		if err = session.StartTransaction(); err != nil {
-			log.Panic(err)
-		}
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
-		if err = mongo.WithSession(ctx, session, func(sc mongo.SessionContext) error {
-			var newBid model.BidWrite
-			err := c.Bind(&newBid)
-			if err != nil {
-				log.Panic(err)
-				return err
-			}
-			_, err = b.bid.InsertOne(nil, newBid)
-			if err != nil {
-				log.Panic(err)
-				return err
-			}
-			payload := model.PawnUpdate{
-				Bid: newBid.ID,
-			}
-			err = b.pawn.UpdateOneBy(nil, "id", newBid.Pawn, payload)
-			if err != nil {
-				log.Panic(err)
-				return err
-			}
-			if err = session.CommitTransaction(sc); err != nil {
-				log.Panic(err)
-				return err
-			}
-			return nil
-		}); err != nil {
-			log.Panic(err)
-		}
-		session.EndSession(ctx)
-	}
-	return fn
 }
